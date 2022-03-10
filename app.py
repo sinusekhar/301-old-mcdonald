c62c1bc655379fe5a47edbeee87322ff2d50fe3b
Sinu Sekhar <sinusekhar@gmail.com>
2022-03-10 01:23:04 +0000

    added an options select

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ list_of_columns =['code', 'state', 'category', 'total exports', 'beef', 'pork', 
        'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
        'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
 
-mycolumn='wheat'
-myheading1 = f"Wow! That's a lot of {mycolumn}!"
-mygraphtitle = '2011 US Agriculture Exports by State'
-mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
-mycolorbartitle = "Millions USD"
+#mycolumn='wheat'
+#myheading1 = f"Wow! That's a lot of {mycolumn}!"
+#mygraphtitle = '2011 US Agriculture Exports by State'
+#mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
+#mycolorbartitle = "Millions USD"
 tabtitle = 'Old McDonald'
 sourceurl = 'https://plot.ly/python/choropleth-maps/'
 githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
@@ -26,20 +26,20 @@ githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
 import pandas as pd
 df = pd.read_csv('assets/usa-2011-agriculture.csv')
 
-fig = go.Figure(data=go.Choropleth(
-    locations=df['code'], # Spatial coordinates
-    z = df[mycolumn].astype(float), # Data to be color-coded
-    locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = mycolorscale,
-    colorbar_title = mycolorbartitle,
-))
+#fig = go.Figure(data=go.Choropleth(
+#    locations=df['code'], # Spatial coordinates
+#    z = df[mycolumn].astype(float), # Data to be color-coded
+#    locationmode = 'USA-states', # set of locations match entries in `locations`
+#    colorscale = mycolorscale,
+#    colorbar_title = mycolorbartitle,
+#))
 
-fig.update_layout(
-    title_text = mygraphtitle,
-    geo_scope='usa',
-    width=1200,
-    height=800
-)
+#fig.update_layout(
+#    title_text = mygraphtitle,
+#    geo_scope='usa',
+#    width=1200,
+#    height=800
+#)
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -50,16 +50,48 @@ app.title=tabtitle
 ########### Set up the layout
 
 app.layout = html.Div(children=[
-    html.H1(myheading1),
-    dcc.Graph(
-        id='figure-1',
-        figure=fig
-    ),
+    html.H1('2011 Agricultural Exports, by State'),
+    html.H3('Select a variable for analysis:'),
+    dcc.Dropdown(
+                    id='options-drop',
+                    options=[{'label': i, 'value': i} for i in list_of_columns],
+                    value='corn'
+                ),
+    html.Div([dcc.Graph(id='figure-1')
+#    dcc.Graph(
+#        id='figure-1',
+#        figure=fig
+#    ),
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A("Data Source", href=sourceurl),
     ]
 )
+
+# make a function that can intake any varname and produce a map.
+@app.callback(Output('figure-1', 'figure'),
+             [Input('options-drop', 'value')])
+def make_figure(varname):
+    mygraphtitle = f'Exports of {varname} in 2011'
+    mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
+    mycolorbartitle = "Millions USD"
+
+    data=go.Choropleth(
+        locations=df['code'], # Spatial coordinates
+        locationmode = 'USA-states', # set of locations match entries in `locations`
+        z = df[varname].astype(float), # Data to be color-coded
+        colorscale = mycolorscale,
+        colorbar_title = mycolorbartitle,
+    )
+    fig = go.Figure(data)
+    fig.update_layout(
+        title_text = mygraphtitle,
+        geo_scope='usa',
+        width=1200,
+        height=800
+    )
+    return fig
+
 
 ############ Deploy
 if __name__ == '__main__':
